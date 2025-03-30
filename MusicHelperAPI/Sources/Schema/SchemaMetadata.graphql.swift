fb7e113c73fa60de5470cf81968bed663ed75f91
Moritz Beyer <lane_dried.0m@icloud.com>
2025-03-30 15:23:35 +0200

    Add API and first simple query

---
+++ MusicHelperAPI/Sources/Schema/SchemaMetadata.graphql.swift
@@ -0,0 +1,32 @@
+// @generated
+// This file was automatically generated and should not be edited.
+
+import ApolloAPI
+
+public protocol SelectionSet: ApolloAPI.SelectionSet & ApolloAPI.RootSelectionSet
+where Schema == MusicHelperAPI.SchemaMetadata {}
+
+public protocol InlineFragment: ApolloAPI.SelectionSet & ApolloAPI.InlineFragment
+where Schema == MusicHelperAPI.SchemaMetadata {}
+
+public protocol MutableSelectionSet: ApolloAPI.MutableRootSelectionSet
+where Schema == MusicHelperAPI.SchemaMetadata {}
+
+public protocol MutableInlineFragment: ApolloAPI.MutableSelectionSet & ApolloAPI.InlineFragment
+where Schema == MusicHelperAPI.SchemaMetadata {}
+
+public enum SchemaMetadata: ApolloAPI.SchemaMetadata {
+  public static let configuration: any ApolloAPI.SchemaConfiguration.Type = SchemaConfiguration.self
+
+  public static func objectType(forTypename typename: String) -> ApolloAPI.Object? {
+    switch typename {
+    case "Playlist": return MusicHelperAPI.Objects.Playlist
+    case "Query": return MusicHelperAPI.Objects.Query
+    default: return nil
+    }
+  }
+}
+
+public enum Objects {}
+public enum Interfaces {}
+public enum Unions {}
