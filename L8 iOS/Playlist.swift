edc0b976222ee7d7c779821c1380743ef5fa6b4d
Moritz Beyer <lane_dried.0m@icloud.com>
2025-04-02 13:15:28 +0200

    Create playlist swift

---
+++ L8 iOS/Playlist.swift
@@ -0,0 +1,13 @@
+//
+//  Playlist.swift
+//  L8 iOS
+//
+//  Created by Moritz Beyer on 02.04.25.
+//
+
+struct Playlist {
+    // var id
+    var description: String
+    var name: String
+    var tracks: [Track]
+}
