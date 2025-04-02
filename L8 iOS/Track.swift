ba40618d8a229cb2fc19195b8dba5900738c7838
Moritz Beyer <lane_dried.0m@icloud.com>
2025-04-02 13:15:42 +0200

    Create track type

---
+++ L8 iOS/Track.swift
@@ -0,0 +1,15 @@
+//
+//  Track.swift
+//  L8 iOS
+//
+//  Created by Moritz Beyer on 02.04.25.
+//
+
+
+struct Track {
+    // var id
+    var durationMs: Int
+    var explicit: Bool
+    var name: String
+    var uri: String
+}
