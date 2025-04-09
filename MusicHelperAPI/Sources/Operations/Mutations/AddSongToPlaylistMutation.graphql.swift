c0cac81786bcdfe68b44336c7b92f53b9a580d6f
Moritz Beyer <lane_dried.0m@icloud.com>
2025-04-09 11:46:36 +0200

    modify graphql mutation

--- MusicHelperAPI/Sources/Operations/Mutations/AddSongToPlaylistMutation.graphql.swift
+++ MusicHelperAPI/Sources/Operations/Mutations/AddSongToPlaylistMutation.graphql.swift
@@ -7,7 +7,7 @@ public class AddSongToPlaylistMutation: GraphQLMutation {
   public static let operationName: String = "addSongToPlaylist"
   public static let operationDocument: ApolloAPI.OperationDocument = .init(
     definition: .init(
-      #"mutation addSongToPlaylist($input: AddItemsToPlaylistInput!) { addItemsToPlaylist(input: $input) { __typename code message playlist { __typename tracks { __typename durationMs explicit id name uri } } } }"#
+      #"mutation addSongToPlaylist($input: AddItemsToPlaylistInput!) { addItemsToPlaylist(input: $input) { __typename code message success } }"#
     ))
 
   public var input: AddItemsToPlaylistInput
@@ -42,61 +42,15 @@ public class AddSongToPlaylistMutation: GraphQLMutation {
         .field("__typename", String.self),
         .field("code", Int.self),
         .field("message", String.self),
-        .field("playlist", Playlist?.self),
+        .field("success", Bool.self),
       ] }
 
       /// Similar to HTTP status code, represents the status of the mutation.
       public var code: Int { __data["code"] }
       /// Human-readable message for the UI.
       public var message: String { __data["message"] }
-      /// The playlist that contains the newly added items.
-      public var playlist: Playlist? { __data["playlist"] }
-
-      /// AddItemsToPlaylist.Playlist
-      ///
-      /// Parent Type: `Playlist`
-      public struct Playlist: MusicHelperAPI.SelectionSet {
-        public let __data: DataDict
-        public init(_dataDict: DataDict) { __data = _dataDict }
-
-        public static var __parentType: any ApolloAPI.ParentType { MusicHelperAPI.Objects.Playlist }
-        public static var __selections: [ApolloAPI.Selection] { [
-          .field("__typename", String.self),
-          .field("tracks", [Track].self),
-        ] }
-
-        /// The tracks in the playlist.
-        public var tracks: [Track] { __data["tracks"] }
-
-        /// AddItemsToPlaylist.Playlist.Track
-        ///
-        /// Parent Type: `Track`
-        public struct Track: MusicHelperAPI.SelectionSet {
-          public let __data: DataDict
-          public init(_dataDict: DataDict) { __data = _dataDict }
-
-          public static var __parentType: any ApolloAPI.ParentType { MusicHelperAPI.Objects.Track }
-          public static var __selections: [ApolloAPI.Selection] { [
-            .field("__typename", String.self),
-            .field("durationMs", Int.self),
-            .field("explicit", Bool.self),
-            .field("id", MusicHelperAPI.ID.self),
-            .field("name", String.self),
-            .field("uri", String.self),
-          ] }
-
-          /// The track length in milliseconds.
-          public var durationMs: Int { __data["durationMs"] }
-          /// Whether or not the track has explicit lyrics (true = yes it does; false = no it does not OR unknown)
-          public var explicit: Bool { __data["explicit"] }
-          /// The ID for the track.
-          public var id: MusicHelperAPI.ID { __data["id"] }
-          /// The name of the track.
-          public var name: String { __data["name"] }
-          /// The URI for the track, usually a Spotify link.
-          public var uri: String { __data["uri"] }
-        }
-      }
+      /// Indicates whether the mutation was successful.
+      public var success: Bool { __data["success"] }
     }
   }
 }
